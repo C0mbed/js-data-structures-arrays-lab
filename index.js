9105aae897190b6b93ae65b9277635a08e8b2d64
C0mbed <matt@farncombe.co>
2021-01-27 13:04:43 -0700

    commit tree 1dc0c555c05591312fc8e88f0ba3e5d02f5c4693

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+    return drivers;
+};
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+    return drivers;
+};
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop();
+    return drivers;
+};
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift();
+    return drivers;
+};
+
+function appendDriver(name) {
+    const newArray = [...drivers, name];
+    return newArray;
+};
+
+function prependDriver(name) {
+    const newArray = [name, ...drivers];
+    return newArray;
+};
+
+function removeLastDriver() {
+    let newArray = drivers.slice(0, drivers.length-1);
+    return newArray;
+};
+
+function removeFirstDriver() {
+    let newArray = drivers.slice(1);
+    return newArray; 
+};
